19ad3a21c3cc67706818a65258073ab66301bf81
Julius Pfadt <julius.pfadt@gmail.com>
2025-08-14 15:17:31 +0200

    fix covariance input bug

--- R/confirmatoryfactoranalysis.R
+++ R/confirmatoryfactoranalysis.R
@@ -32,7 +32,9 @@ confirmatoryFactorAnalysisInternal <- function(jaspResults, dataset, options, ..
   errors <- .cfaCheckErrors(dataset, options)
 
   # possibly check cov matrix
-  .pcaAndEfaDataCovarianceCheck(dataset, options, ready = TRUE, cfa = TRUE)
+  .pcaAndEfaDataCovarianceCheck(dataset, options,
+                                ready = length(unlist(lapply(options[["factors"]], `[[`, "indicators"), use.names = FALSE) > 0),
+                                cfa = TRUE)
 
   # Main table / model
   cfaResult <- .cfaComputeResults(jaspResults, dataset, options, errors)
@@ -102,8 +104,12 @@ confirmatoryFactorAnalysisInternal <- function(jaspResults, dataset, options, ..
     dataset <- dataset[, vars]
 
   } else {
-    colInds <- which(colnames(dataset) == vars)
-    dataset <- dataset[colInds, colInds]
+    columnIndices <- sapply(vars, jaspBase:::columnIndexInData) + 1 # cpp starts at 0
+    # reorder the dataset columns because the columnIndices are determined based on the "unloaded" data,
+    # meaning the loaded data columns are ordered somewhat alphabetically
+    dataset <- dataset[, names(columnIndices)]
+    dataset <- dataset[columnIndices, ]
+    dataset[] <- lapply(dataset, function(x) as.numeric(as.character(x)))
     rownames(dataset) <- colnames(dataset)
   }
 
@@ -131,7 +137,6 @@ confirmatoryFactorAnalysisInternal <- function(jaspResults, dataset, options, ..
 
   return(options)
 }
-
 
 
 .cfaCheckErrors <- function(dataset, options) {
